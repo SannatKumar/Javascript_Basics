1759e69fe8be2e5f6a426fcd56432427ef487ef6
Raj Kumar <interplaytheheads@gmail.com>
2019-09-30 14:01:33 +0300

    Primitive and Reference types variable examples updated along with Readme.txt theory

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Here is javascript code.
+/*//Here is javascript code.
 
 console.log('Hello Existence');
 
@@ -17,15 +17,41 @@ console.log(interestRate);// later one is assigned and executed.
 //value of a constant cannot be changed.
 const salaryRate = 1;
 
-salaryRate = 2;// This gives error because const cannot be changed.
+//salaryRate = 2; This gives error because const cannot be changed.
 console.log(salaryRate); 
 
 //Examples of primitive variable types
-let name = 'Raj Kumar'; //String Literal
+let userName = 'Raj Kumar'; //String Literal
 let age = 30; //Number Literal
 let isApproved = false; // Boolean Literal
 let firstName;// Without intialization it is undefined 
-let firstName = undefined; //but we can defined explicitly. 
-let lastName = null; // if we want to explicitly clear out the variable.
+let anotherName = undefined; //but we can defined explicitly. 
+let selectedColor = null; // if we want to explicitly clear out the variable.
+
+*/
+//Examples of Reference types
+
+//Object
+
+//Person is a object literal
+
+let person = {
+    name : 'Raj Kumar',
+    age : 30
+};
+
+console.log(person);
+
+//Accessing the properties
+//Dot Notation
+person.name = 'John';
+
+console.log(person.name);
+
+//Bracket Notation
+
+person['name'] = 'Mary';
+
+console.log(person.name);
 
 
