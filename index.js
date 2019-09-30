b69b9f44e7d2d76ae31169fc2d0b6878e1631a61
Raj Kumar <interplaytheheads@gmail.com>
2019-09-30 13:20:05 +0300

    variables use updated

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 //Here is javascript code.
 
 console.log('Hello Existence');
+
+//Declaring variables
+
+let name = 'Raj Kumar';
+
+console.log(name);
+
+
+let interestRate = 0.3;
+interestRate = 1;
+
+console.log(interestRate);// later one is assigned and executed.
+
+//value of a constant cannot be changed.
+const salaryRate = 1;
+
+salaryRate = 2;// This gives error because const cannot be changed.
+console.log(salaryRate); 
+
