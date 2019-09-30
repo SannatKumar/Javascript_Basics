d94a6db9c3dbc1a2a07c5489846f089bef804fd1
Raj Kumar <interplaytheheads@gmail.com>
2019-09-30 13:35:37 +0300

    Primitive variable example type along with Readme.ms theory

--- index.js
+++ index.js
@@ -20,3 +20,12 @@ const salaryRate = 1;
 salaryRate = 2;// This gives error because const cannot be changed.
 console.log(salaryRate); 
 
+//Examples of primitive variable types
+let name = 'Raj Kumar'; //String Literal
+let age = 30; //Number Literal
+let isApproved = false; // Boolean Literal
+let firstName;// Without intialization it is undefined 
+let firstName = undefined; //but we can defined explicitly. 
+let lastName = null; // if we want to explicitly clear out the variable.
+
+
