d3a9a1f04954f7b93d6ec9c8042f30f19d8e9892
Raj Kumar <interplaytheheads@gmail.com>
2019-10-01 12:07:29 +0300

    Arrays Examples

---
+++ arrays.js
@@ -0,0 +1,16 @@
+//Arrays example
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 'green';
+selectedColors[3] = 1;
+//Accessing all the values from array
+console.log(selectedColors);
+
+
+//Accessing all the values from index
+console.log(selectedColors[0]);
+
+//Accessing the arrays length
+console.log(selectedColors.length);
+
+
+
