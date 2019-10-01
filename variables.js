4fe6321cfc2e9fa373fcba8add09b3c4c3d69291
Raj Kumar <interplaytheheads@gmail.com>
2019-10-01 12:09:47 +0300

    variables examples

---
+++ variables.js
@@ -0,0 +1,57 @@
+/*//Here is javascript code.
+
+console.log('Hello Existence');
+
+//Declaring variables
+
+let name = 'Raj Kumar';
+
+console.log(name);
+
+
+let interestRate = 0.3;
+interestRate = 1;
+
+console.log(interestRate);// later one is assigned and executed.
+
+//value of a constant cannot be changed.
+const salaryRate = 1;
+
+//salaryRate = 2; This gives error because const cannot be changed.
+console.log(salaryRate); 
+
+//Examples of primitive variable types
+let userName = 'Raj Kumar'; //String Literal
+let age = 30; //Number Literal
+let isApproved = false; // Boolean Literal
+let firstName;// Without intialization it is undefined 
+let anotherName = undefined; //but we can defined explicitly. 
+let selectedColor = null; // if we want to explicitly clear out the variable.
+
+*/
+//Examples of Reference types
+
+//Object
+
+//Person is a object literal
+
+let person = {
+    name : 'Raj Kumar',
+    age : 30
+};
+
+console.log(person);
+
+//Accessing the properties
+//Dot Notation
+person.name = 'John';
+
+console.log(person.name);
+
+//Bracket Notation
+
+person['name'] = 'Mary';
+
+console.log(person.name);
+
+
